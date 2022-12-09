789df7e97263a06a64c68d22a9336aa458792bdb
rishikesh312 <r.rishikesh.rr@gmail.com>
2022-12-09 22:48:51 +0100

    work in progress

--- Q11.py
+++ Q11.py
@@ -8,10 +8,9 @@ Created on Thu Dec  8 20:39:34 2022
 import torchvision
 import torch
 import torchvision.transforms as transforms
-import matplotlib.pyplot as plt
-import numpy as np
 import torch.nn as nn
-img_size=19
+
+#loads the dataset of variable resolution
 transform = transforms.Compose(
 [transforms.ToTensor()])
 path="mnist-varres"
@@ -19,22 +18,19 @@ training_dataset = torchvision.datasets.ImageFolder(root=path,
                                                     transform=transform)
 train_load = torch.utils.data.DataLoader(training_dataset,batch_size=1,num_workers=0,shuffle=True)
 
-def imshow(img):
-    img = img / 2 + 0.5     # unnormalize
-    npimg = img.numpy()
-    plt.imshow(np.transpose(npimg, (1, 2, 0)))
-    plt.show()
 
 
 # get some random training images
 dataiter = iter(train_load)
 images, labels = next(dataiter)
+#prints the image resolution in tensor format
 print(images.size())
 #img=T.ToTensor()()
-# show images
+
+
+"""Still not complete"""
 globalmax=nn.MaxPool2d(2,2)
 globavg=nn.AvgPool2d(1,1)
-
 t1=globalmax(images)
 t=globavg(t1)
 print(t1.size())
